1a65d241ee4457ddcef6cea437506e590c843920
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2020-03-06 21:47:27 -0300

    Fixed some errors

--- models/noteModel.js
+++ models/noteModel.js
@@ -2,21 +2,18 @@ module.exports = (sequelize, type) => {
     const Note = sequelize.define('note', {
         Id: {
             type: type.INTEGER
-        }
+        },
         Filename: {
             type: type.STRING
-        }
+        },
         Extension: {
             type: type.STRING
-        }
+        },
         Description: {
             type: type.STRING
-        }
+        },
         Url: {
             type: type.STRING
-        }
-        AssignatureId: {
-            type: type.INTEGER
         }
     })
     return Note
